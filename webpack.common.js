d81307930428da208a352328a347d7bd64b0e598
Diego-Della-Vecchia <155764085+Diego-Della-Vecchia@users.noreply.github.com>
2024-04-13 13:34:22 +0200

    Initial commit

---
+++ webpack.common.js
@@ -0,0 +1,38 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "src/index.html"),
+      filename: "index.html",
+      inject: "head",
+      scriptLoading: "defer",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg|woff|woff2|eot|ttf|otf)$/,
+        type: "asset/resource",
+        generator: {
+          filename: "assets/[name][ext]",
+        },
+      },
+      {
+        test: /\.html$/i,
+        use: ["html-loader"],
+      },
+    ],
+  },
+};
